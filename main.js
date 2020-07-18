5c48f11a85259c644031146a71a3f0eb35a72cf6
Adam Kearn <adamkearn17@gmail.com>
2020-07-18 17:18:24 +0100

    Ability to select the Media Folders
    I have added the ability to select the media folders to download trailers for.
    I've used the enquirer package to do this as it was the simplest to setup/use.
    
    All of the selected media folder items get put into one big array that will then get iterated over.

--- main.js
+++ main.js
@@ -1,7 +1,37 @@
+const { MultiSelect } = require('enquirer');
+
 const { language } = require('./config');
-const { getItemsFromParentID } = require('./lib/embyAPIRequests.js');
+const { getMediaFolders, getItemsFromParentID } = require('./lib/embyAPIRequests.js');
 const { getTrailerKey } = require('./lib/tmdbAPIRequests.js');
 const { getTrailer } = require('./lib/trailerHandler.js');
+
+function selectMediaFolders() {
+  return new Promise(async function(resolve, reject) {
+    const mediaFolders = await getMediaFolders();
+    const choices = []
+
+    for (const folder in mediaFolders) {
+      choices.push({
+        name: mediaFolders[folder].Name,
+        value: mediaFolders[folder].Id
+      });
+    }
+
+    const prompt = new MultiSelect({
+      message: 'Select the media folders that you would like to download trailers for:',
+      choices: choices,
+      result(names) {
+        if (names.length <= 0) { reject(new Error('You MUST select at least one media folder.')); }
+
+        return Object.values(this.map(names));
+      }
+    });
+
+    prompt.run()
+    .then(answers => resolve(answers))
+    .catch(console.error);
+  });
+}
 
 // I didn't know how to do this.
 // Source: https://codeburst.io/javascript-async-await-with-foreach-b6ba62bbf404
@@ -11,11 +41,32 @@ async function asyncForEach(array, callback) {
   }
 }
 
+async function getSelectedFoldersItems(mediaFolders) {
+  return new Promise(async function(resolve, reject) {
+    const allItems = [];
+
+    for (const folder of mediaFolders) {
+      const folderItems = await getItemsFromParentID(folder);
+
+      await asyncForEach(folderItems, async (item) => {
+        allItems.push({
+          name: item.Name,
+          type: item.Type,
+          tmdb: item.ProviderIds.Tmdb
+        });
+      });
+    }
+
+    resolve(allItems);
+  });
+}
+
 const start = async () => {
-  // Hard coded for now.
-  const items = await getItemsFromParentID('b8bb41f1a97845838e5f6494486d7b62');
+  const mediaFolders = await selectMediaFolders();
+  const items = await getSelectedFoldersItems(mediaFolders)
+
   await asyncForEach(items, async (item) => {
-    const key = await getTrailerKey(item.Type, item.ProviderIds.Tmdb, language);
+    const key = await getTrailerKey(item.type, item.tmdb, language);
     const trailer = await getTrailer(key, "path");
 
     if (trailer) { console.log('\n' + '='.repeat(30) + '\n'); }
